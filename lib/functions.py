e12c765fc88380a9c53a1b2c062c5acab50eb873
Yetunde Alese <yaalese1@gmail.com>
2023-07-26 15:13:23 -0400

    done

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,24 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello {name}!")
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    if not (number,(int)):
+        return None 
+    return number/2
+
+#the if statment was not needed but if you want to allow for less errors if a decimal is entered orbullet proof code it helps 
